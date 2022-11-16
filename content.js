60b861afc59f7015968fcba0047e0399ba065980
Uliana Lobanova <uliana.lobanova@binary-studio.com>
2022-11-16 16:47:51 +0200

    test custom notification

--- content.js
+++ content.js
@@ -1,4 +1,17 @@
-(() => {
+const showMessage = () => {
+  const message = 'Copied to clipboard';
+
+  let toastContainer = document.createElement("div");
+  toastContainer.innerText = message;
+  toastContainer.className = "vfx-extension-toast";
+  document.body.appendChild(toastContainer);
+  
+  // setTimeout(()=>{
+  //     document.body.removeChild(toastContainer);
+  // }, 2000);
+};
+
+const copyContentToClipboard = () => {
   const items = document.querySelectorAll('a[sg_selector="link:sg_detail"][sg_entity_type="Shot"]');
   const itemsContent = [];
 
@@ -7,7 +20,12 @@
   }
 
   const formattedContent = itemsContent.join('\n');
-  navigator.clipboard.writeText(formattedContent).then(() => {
-    chrome.runtime.sendMessage('Copied to clipboard');
-  })
-})();
+
+  navigator.clipboard.writeText(formattedContent).then(showMessage);
+};
+
+chrome.runtime.onMessage.addListener(request => {
+  if (request.extIconPressed) {
+    copyContentToClipboard();
+  }  
+});
